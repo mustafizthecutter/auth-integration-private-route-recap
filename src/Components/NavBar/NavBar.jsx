609eb84f527288c345f95c2f538e35ed2aa22057
Mustafizur Rahman <mustafizur232@gmail.com>
2024-01-01 18:46:03 +0600

    login and registration form created with the help of daisyUi

--- src/Components/NavBar/NavBar.jsx
+++ src/Components/NavBar/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
                         {navLinks}
                     </ul>
                 </div>
-                <a className="btn btn-ghost text-xl">daisyUI</a>
+                <a className="btn btn-ghost text-xl">Auth Combination Recap</a>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
